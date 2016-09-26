111ef3ec8e3197106b95cdf4db0d9e20cf6a26b6
onTheexcited <1046042736@qq.com>
2016-09-26 10:07:06 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/bistu/cs/se/graphic30/MainActivity.java
@@ -0,0 +1,13 @@
+package cn.edu.bistu.cs.se.graphic30;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sample_my_view);
+    }
+}
